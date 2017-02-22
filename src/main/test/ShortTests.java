70277034e60f9344463f55d2d737a4c0c95bf44d
Alisa Demennikova <a.demennikova@andersenlab.com>
2017-02-22 18:23:45 +0200

    Add correct structure for project. Fix your pom.xml file.

---
+++ src/main/test/ShortTests.java
@@ -0,0 +1,50 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * ShortTests Tester.
+ *
+ * @author <Alisa Demennikova>
+ * @version 1.0
+ * @since <pre>Feb 22, 2017</pre>
+ */
+public class ShortTests {
+    @Before
+    public void before() throws Exception {
+    }
+
+    @Test
+    public void shortTaskTest1() {
+
+    }
+
+    @Test
+    public void shortTaskTest2() {
+
+    }
+
+    @Test
+    public void shortTaskTest3() {
+
+    }
+
+    @Test
+    public void shortTaskTest4() {
+
+    }
+
+    @Test
+    public void shortTaskTest5() {
+
+    }
+
+    @Test
+    public void shortTaskTest6() {
+
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+}
